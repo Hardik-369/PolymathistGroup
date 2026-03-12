a775475c8d00745222ff41cbe5c642a18093ac3c
Hardik Kawale <142771505+Hardik-369@users.noreply.github.com>
2026-03-12 22:34:26 +0530

    Update avatar URL for Hardik Kawale

--- src/components/CommunitySection.tsx
+++ src/components/CommunitySection.tsx
@@ -11,7 +11,7 @@ const members = [
     name: "Hardik Kawale", 
     role: "Creative Dev", 
     skills: "Python/ AI / WebDev / Three.js", 
-    avatar: "/images/Hardik.png",
+    avatar: "https://github.com/Hardik-369/PolymathistGroup/blob/main/Public/images/Hardik.png",
     color: "#FF0055",
     socials: { github: "https://github.com/Hardik-369", instagram: "https://www.instagram.com/__shadow__4444/", linkedin: "https://www.linkedin.com/in/hardikkawale" }
   },
